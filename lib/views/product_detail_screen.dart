0cff860d9eb2d34db9ff64eea0d0c297019f4fa9
Junior da Silva Pereira <junior_silvapereira@hotmail.com>
2024-05-12 17:00:36 -0300

    feat: product detail screen

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -14,6 +14,41 @@ class ProductDetailScreen extends StatelessWidget {
         title: Text(product.title),
         backgroundColor: Theme.of(context).primaryColor,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'R\$ ${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
